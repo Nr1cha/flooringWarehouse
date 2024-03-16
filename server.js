46dc3d570dacaeabb9911c734104e464dc1b2716
Nr1cha <vrm.richards@gmail.com>
2024-03-15 21:49:26 -0600

    renaming

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const { connectToServer } = require("./db");
+const { connectToServer } = require("./db/db");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 const cors = require("cors");
