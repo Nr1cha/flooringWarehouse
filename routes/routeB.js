fd62f5b2752c88a99b6bf4f1af404fb901ede077
Nick Richards <vrm.richards@gmail.com>
2024-03-20 14:31:56 -0600

    added new route

--- routes/routeB.js
+++ routes/routeB.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const bookRoutes = require("./bookRoutes");
-const memberRoutes = require("./memberRoutes");
-const swaggerUI = require("swagger-ui-express");
-const swaggerConfig = require(`../swagger-output-${process.env.CURRENT_LANE}.json`);
+const {
+    getAllCustomers,
+    updateCustomer,
+    getSingleCustomer,
+    setSingleCustomer,
+    deleteSingleCustomer
+} = require('../controllers/customerController');
+const { CustomerIdSchema,
+    validateCustomerPost
+} = require('../middleware/validation');
 
-router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerConfig));
-router.use('/books', bookRoutes);
-router.use('/members', memberRoutes);
+// GET request for list of all Member items.
+router.get('/', getAllCustomers);
+
+//GET single member
+router.get("/:id", CustomerIdSchema, getSingleCustomer);
+
+// POST request for creating Member.
+router.post('/:id', validateCustomerPost, updateCustomer);
+
+//put route
+router.put('/create', validateCustomerPost, setSingleCustomer);
+
+//delete route
+router.delete('/:id', CustomerIdSchema, deleteSingleCustomer);
 
 module.exports = router;
