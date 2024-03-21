f5b0fa48be50f2923bbe1c58a23f3be8a8c158df
Nr1cha <vrm.richards@gmail.com>
2024-03-21 15:29:33 -0600

    added orders

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const productRoutes = require("./products");
 const customerRoutes = require("./customer");
 const employeeRoutes = require("./employee");
+const ordersRoutes = require("./orders");
 const swaggerUi = require('swagger-ui-express');
 // const swaggerDocument = require('./swagger.json');
 const swaggerDocument = require(`../swagger-output-${process.env.CURRENT_LANE}.json`);
@@ -13,8 +14,7 @@ router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use("/floorings", productRoutes);
 router.use("/customers", customerRoutes);
 router.use("/employees", employeeRoutes);
-
-
+router.use("/orders", ordersRoutes);
 
 
 module.exports = router;
