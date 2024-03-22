f8c4c272be9ea57d15304f81975e2fd1545e5310
Nick Richards <vrm.richards@gmail.com>
2024-03-22 14:43:56 -0600

    testing

--- routes/customer.test.js
+++ routes/customer.test.js
@@ -1,7 +1,9 @@
 const request = require('supertest');
 const express = require('express');
 const customer = require('./customer');
+const db = require('../db/index')
 
+jest.mock('../db');
 
 const app = express();
 app.use(express.json());
@@ -9,7 +11,25 @@ app.use('/', customer);
 
 describe('Verify the functionality of the /customers path', () => {
     test('The GET method should return a successful response', async () => {
+
+        const mockGetData = jest.spyOn(db, "getDb").mockImplementation(() => {
+            return {
+                collection: () => {
+                    return {
+                        find: () => {
+                            return {
+                                toArray: () => {
+                                    return ['nick']
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual(['nick']);
     });
 });
